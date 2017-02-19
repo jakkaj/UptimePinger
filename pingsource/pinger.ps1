c8ced3d4a29b5e03c8e964c82b30fbb16acea08d
Jordan Knight <jorkni@microsoft.com>
2017-02-19 13:17:00 +1100

    Added pinger script

---
+++ pingsource/pinger.ps1
@@ -0,0 +1,4 @@
+﻿Invoke-WebRequest -UseBasicParsing `
+https://functionplayground.azurewebsites.net/api/InternetUpLogger?code=SBCl2hu2Cli1xSWmzRjCe5KPomx5bdF8zSaGaFpThIH6c6tdXkmHlw== `
+ -ContentType "application/json" -Method POST -Body `
+ "{ 'MachineName':'Beastly'}"
